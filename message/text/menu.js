11783edff6732285cececf970c84a801b544caea
HungXiao007 <heikalsyahshiddiq@gmail.com>
2020-10-24 11:00:37 +0700

    Nambahin text formatting

--- message/text/menu.js
+++ message/text/menu.js
@@ -1,26 +1,26 @@
 exports.textMenu = (pushname) => {
     return `
-Halo ${pushname}~
+Halo *${pushname}*~
 
 Command list:
-1. $say
+1. *$say*
 Ya gitu.
 Aliases: -
 Penggunaan: $say <teks>
 
-2. $ping
+2. *$ping*
 Cek speed bot, bukan koneksi kamu.
-Aliases: p
+Aliases: *p*
 Penggunaan: $ping
 
-3. $help
+3. *$help*
 Cek command list.
 Aliases: -
 Penggunaan: $menu
 
-4. $sticker
+4. *$sticker*
 Bikin stiker dari gambar yang dikirim atau di-reply.
-Aliases: stiker
+Aliases: *stiker*
 Penggunaan: Kirim gamber dengan caption $sticker atau reply gambar dengan caption $sticker.
     `
 }
