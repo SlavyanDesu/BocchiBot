5e2e181d52999865773e992f4f13386376e38e12
Heikal Syah Shiddiq <slavyan@secret.fyi>
2021-01-18 16:00:46 +0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,16 @@ module.exports = {
         "ecmaVersion": 12
     },
     "rules": {
+        "no-empty": "warn",
+        "no-extra-semi": "warn",
+        "no-unexpected-multiline": "warn",
+        "default-case": "error",
+        "default-case-last": "error",
+        "no-alert": "warn",
+        "no-empty-function": "warn",
+        "no-invalid-this": "warn",
+        "no-useless-catch": "warn",
+        "require-await": "error",
+        "quotes": ["warn", "single"]
     }
 };
