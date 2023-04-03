180fc02b4bbd7ce6910e2ff9244d201a9b27a493
Piyoxz <102013178+Piyoxz@users.noreply.github.com>
2023-04-04 00:33:12 +0700

    Create quizizz.js
    added: QUIZIZZ (Hack Answers)

---
+++ function/quizizz.js
@@ -0,0 +1,25 @@
+
+
+/**
+ * @typedef QuizizzHack
+ * @property {string} answers
+ */
+
+/**
+ * Quizizz Hack Tools.
+ * @param {string} pin
+ * @returns {Promise<QuizizzData>} answers
+ */
+
+const quizizz = (pin) => new Promise((resolve, reject) => {
+  if (!pin) return reject('Require Pin Query.')
+  let obj = {
+    status: 200
+    url: `https://piyo.my.id/quizizz?pin=${pin}`
+  }
+  resolve(obj)
+})
+
+module.exports = {
+  quizizz
+}
