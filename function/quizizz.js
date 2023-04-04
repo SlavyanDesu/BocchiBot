b4717f081f98d97b85879b16cccc3621d644e8fa
Piyoxz <102013178+Piyoxz@users.noreply.github.com>
2023-04-04 19:20:51 +0700

    Update quizizz.js
    change: Method POST

--- function/quizizz.js
+++ function/quizizz.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch')
+
 /**
  * @typedef QuizizzHack
  * @property {string} answers
@@ -11,11 +13,17 @@
 
 const quizizz = (pin) => new Promise((resolve, reject) => {
   if (!pin) return reject('Require Pin Query.')
-  let obj = {
-    status: 200,
-    url: `https://piyo.my.id/quizizz?pin=${pin}`
-  }
-  resolve(obj)
+  const Data = await fetch('https://piyo.my.id/hack', {
+        method: 'POST',
+        headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json'
+         },
+        body: JSON.stringify({pin: pin})
+  })
+  const Response = await Data.json();
+  if (Response.status === 400) return reject(Response.message)
+  resolve(Response)
 })
 
 module.exports = {
